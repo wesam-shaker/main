a6f6643723c5dffd39f80f74244f01f39c06d447
wesam-shaker <95919531+wesam-shaker@users.noreply.github.com>
2022-01-10 21:27:44 +0200

    تحميل ملفات الموقع

---
+++ js/my.js
@@ -0,0 +1,11 @@
+const div = document.querySelector(".scroll-top");
+
+    div.addEventListener("click", function () {
+        // window.scrollTo(0, 0);
+            window.scrollTo({
+            top:0,
+            left:0,
+            behavior: "smooth",
+        });
+        // $("html, body").animate({ scrollTop: 0}, "slow");
+    });
